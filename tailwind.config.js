22afd66f13f9c8d82ad0f843f5981dae013fef22
Fayzzi <faizancuiwah@gmail.com>
2024-03-08 09:51:46 +0500

    threejs all done

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#F5385D",
+        lightBlack: "#0000005f",
+        darkRed: "#a70819",
+      },
+      screens: {
+        "1000px": "1050px",
+        "1100px": "1110px",
+        "800px": "800px",
+        "1300px": "1300px",
+        "400px": "400px",
+      },
+    },
+  },
+  plugins: [],
+};
